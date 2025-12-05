430126003ea4996dfe1ae57982f473df5e6c89d4
tanmayrathi-gkmit <tanmayrathi@gkmit.co>
2025-12-05 12:39:23 +0530

    add: github actions workflows

---
+++ test_calc.py
@@ -0,0 +1,30 @@
+import pytest
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+
+
+def test_subtract():
+    assert subtract(3, 2) == 1
+    assert subtract(2, 3) == -1
+    assert subtract(0, 0) == 0
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-2, 3) == -6
+    assert multiply(-2, -3) == 6
+
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(6, -3) == -2
+    assert divide(-6, -3) == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(5, 0)
