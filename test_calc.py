821ffeba84536a04e6bc38d920b05c5de55c6faa
tanmayrathi-gkmit <tanmayrathi@gkmit.co>
2025-12-05 12:46:21 +0530

    test: failed

--- test_calc.py
+++ test_calc.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(1, 2) == 3
+    assert add(1, 2) == 5
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
 
